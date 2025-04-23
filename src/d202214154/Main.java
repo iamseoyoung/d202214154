1c352680a3edacb63ad4c71d8c27b5aeb62d34ac
Lee, Seoyoung <2seo02seo0@naver.com>
2025-04-23 09:30:06 +0900

    five

--- src/d202214154/Main.java
+++ src/d202214154/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("이서영");
 		World.run();
+		World.run2();
 	}
 
 }
--- src/d202214154/World.java
+++ src/d202214154/World.java
@@ -1,1 +1,2 @@
-public class World { public static void run() { System.out.println("202214154"); } }
+public class World { public static void run() { System.out.println("202214154"); }
+public static void run2() { System.out.println("hello world"); } }
